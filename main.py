5f223c62ab1f54dad375959fcb79047bf19b4ca3
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2023-05-22 18:19:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ buffer = io.BytesIO()
 
 #n_persone = 30
 
-def new_assignement(size):
-    return np.random.randint(1, 7, size)
-
 
 if __name__ == '__main__':
 
@@ -25,21 +22,23 @@ if __name__ == '__main__':
         st.write(f'You selected {uploaded_file.name}')
         df = pd.read_excel(uploaded_file).reset_index(drop=True)
         n_sessione = st.number_input("numero di gruppi", min_value=1, format="%d")
-        df["Utente - Luogo"] = df["Utente - Luogo"].str.upper()
-        df["Utente - ID utente"] = df["Utente - ID utente"].str.upper()
-        df = df.sort_values(by="Utente - Luogo")
-        df["session"] =  list(range(1, len(df) + 1))
-        df["session"] =  df["session"].apply(lambda x: x % n_sessione)
-        df["session"] =  df["session"]+1
-        df = df.sort_values(by="session")
-        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-            df.to_excel(writer, sheet_name='Sheet1', index=False)
-           # Close the Pandas Excel writer and output the Excel file to the buffer
-            writer.save()
+        if st.button("run")
+        
+            df["Utente - Luogo"] = df["Utente - Luogo"].str.upper()
+            df["Utente - ID utente"] = df["Utente - ID utente"].str.upper()
+            df = df.sort_values(by="Utente - Luogo")
+            df["session"] =  list(range(1, len(df) + 1))
+            df["session"] =  df["session"].apply(lambda x: x % n_sessione)
+            df["session"] =  df["session"]+1
+            df = df.sort_values(by="session")
+            with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+                df.to_excel(writer, sheet_name='Sheet1', index=False)
+               # Close the Pandas Excel writer and output the Excel file to the buffer
+                writer.save()
 
-            download2 = st.download_button(
-            label="Download data as Excel",
-            data=buffer,
-            file_name='Executed.xlsx',
-            mime='application/vnd.ms-excel'
-            )
+                download2 = st.download_button(
+                label="Download data as Excel",
+                data=buffer,
+                file_name='Executed.xlsx',
+                mime='application/vnd.ms-excel'
+                )
