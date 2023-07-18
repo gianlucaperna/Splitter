69940aa62c025af2d1ee30563b19030139a02cc0
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2023-07-18 17:02:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
         st.write(f'You selected {uploaded_file.name}')
         df = pd.read_excel(uploaded_file).reset_index(drop=True)
         columns = df.columns
-        col_to_group = st.selectbox("Col where to do sort", columns, )
+        col_to_group = st.selectbox("Colonna da usare per fare ordinamento:", columns )
         n_sessione = st.number_input("numero di gruppi", min_value=1, format="%d")
         if st.button("RUN"):
         
